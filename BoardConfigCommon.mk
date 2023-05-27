768d1e56daecdaf3d27ca49ffb23930b71ba68ad
Erfan Abdi <erfangplus@gmail.com>
2023-05-27 14:09:03 +0330

    sm8250-common: Adapt to LMODroid using script
    Change-Id: I5875bf9e441069b1f2616573dffaac9f3b848d0f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -142,7 +142,7 @@ BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 9122611200 # ( BOARD_SUPER_PARTITION_SIZE -
 ifneq ($(WITH_GMS),true)
 $(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := -1))
-SSI_PARTITIONS_RESERVED_SIZE := 1258291200
+SSI_PARTITIONS_RESERVED_SIZE := 1073433600
 else
 SSI_PARTITIONS_RESERVED_SIZE := 30720000
 endif
@@ -224,7 +224,7 @@ endif
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/lmodroid/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc
